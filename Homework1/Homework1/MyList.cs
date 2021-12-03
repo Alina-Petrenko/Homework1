3d5ec5ee363ae474f3514ba618f8b8aa6a475d97
Alina Petrenko <petrenkoaalina@gmail.com>
2021-12-03 17:46:29 +0200

    Added class MyList, methods: (Add, AddRange), interface IEnumerable

---
+++ Homework1/Homework1/MyList.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1
+{
+    class MyList<T> : IEnumerable<T>
+    {
+        private T[] _myList = new T[4];
+        private int _count = 0;
+
+        public T this[int i]
+        {
+            get
+            {
+                if (i < 0 || i >= _count)
+                    throw new IndexOutOfRangeException();
+                return _myList[i];
+            }
+
+            set
+            {
+                if (i < 0 || i >= _count)
+                    throw new IndexOutOfRangeException();
+                _myList[i] = value;
+            }
+        }
+        public void Add(T item)
+        {
+            if (_count >= _myList.Length)
+            {
+                T[] _newList = new T[_myList.Length * 2];
+                for (int i = 0; i< _myList.Length; i++)
+                {
+                    _newList[i] = _myList[i];
+                }
+                _myList = _newList;
+                    
+            }
+            _myList[_count] = item;
+            _count++;
+        }
+
+        public void AddRange( T[] _newList)
+        {
+            foreach (T item in _newList)
+            {
+                Add(item);
+            }
+        }
+        public void AddRange(IEnumerable<T> _newList)
+        {
+            foreach (T item in _newList)
+            {
+                Add(item);
+            }
+        }
+            public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i<_myList.Length; i++)
+            {
+                yield return _myList[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            for (int i = 0; i < _myList.Length; i++)
+            {
+                yield return _myList[i];
+            }
+        }
+    }
+}
--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -7,6 +7,18 @@ namespace Homework1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MyList<int> myList = new MyList<int>();
+            myList.Add(1);
+            myList.Add(5);
+            myList.Add(3);
+            myList.Add(7);
+            myList.Remove(3);
+            foreach (var item in myList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(myList[0]);
+            Console.WriteLine(myList[1]);
         }
     }
 }
